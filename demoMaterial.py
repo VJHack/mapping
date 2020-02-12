fd26aed0a9597602e26cc0cfbd2365127f416738
Win San <wson3124@gmail.com>
2020-02-11 23:43:08 -0600

    Add files via upload

---
+++ demoMaterial.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+# bottom two are used to demonstrate program; swap them into the sample data section
+sampleBarriers = np.array([[-20, 10, -20, -10], [-20, -10, 20, -10]], float)
+sampleObs = np.array([[10, 0, 40], [20, 0, 80], [30, 0, 160]], float)
+sampleState = np.array([0, 0, -np.pi/2])
+
+
+sampleBarriers = np.array([[-10, -20, -10, 20], [-10, 20, 10, 20]], float)
+sampleObs = np.array([[10, 0, 40], [20, 0, 80], [30, 0, 160]], float)
+sampleState = np.array([0, 0, 0])
+
+
+
+
