5bcffe24f6964a5138423b4626fad22b4f010842
Win San <wson3124@gmail.com>
2020-02-11 23:43:57 -0600

    Update demoMaterial.py

--- demoMaterial.py
+++ demoMaterial.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-# bottom two are used to demonstrate program; swap them into the sample data section
+# bottom two are used to demonstrate program; swap them into the sample data section of mappingV5.py
 sampleBarriers = np.array([[-20, 10, -20, -10], [-20, -10, 20, -10]], float)
 sampleObs = np.array([[10, 0, 40], [20, 0, 80], [30, 0, 160]], float)
 sampleState = np.array([0, 0, -np.pi/2])
